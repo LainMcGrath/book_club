35a41b3ae668b9116601fc94d2892631f2d2ef36
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-15 09:32:05 -0700

    Add average number of pages test

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  describe "relationships" do
+    it {should have_many :author_books}
+  end
+end
+
+describe 'average number of pages' do
+  it "can calculate averge number of pages in all books" do
+
+    author_1 = Author.create!(name: "Anthony Doerr")
+
+    book_1 = author_1.books.create!(title: "All the Light We Cannot See",
+                                    pages: 544,
+                                    publication_year: 2014)
+
+    book_2 = author_1.books.create!(title: "About Grace",
+                                    pages: 432,
+                                    publication_year: 2015)
+
+    expect(author_1.avg_number_pages).to eq(488)
+  end
+end
