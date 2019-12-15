8770b29dae14c5b1959736ccf67196f71e293dbb
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-15 09:57:22 -0700

    Add validation tests for books

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-#   describe "validations" do
-#     it {should validate_presence_of :title}
-#     it {should validate_presence_of :publication_year}
-#     it {should validate_presence_of :pages}
-# end
+  describe "validations" do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :pages}
+    it {should validate_presence_of :publication_year}
+end
 
   describe "relationships" do
       it {should have_many :author_books}
