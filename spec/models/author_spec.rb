1e1665142f39d9cba17499c77419a0bdb0a7a404
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-15 09:56:44 -0700

    Add tests for author validation

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+  end
+
   describe "relationships" do
     it {should have_many :author_books}
+    it {should have_many(:books).through(:author_books)}
   end
 end
-<<<<<<< HEAD
 
-describe 'average number of pages' do
-  it "can calculate averge number of pages in all books" do
+  describe 'average number of pages' do
+    it "can calculate averge number of pages in all books" do
 
-    author_1 = Author.create!(name: "Anthony Doerr")
+      author_1 = Author.create!(name: "Anthony Doerr")
 
-    book_1 = author_1.books.create!(title: "All the Light We Cannot See",
-                                    pages: 544,
-                                    publication_year: 2014)
+      book_1 = author_1.books.create!(title: "All the Light We Cannot See",
+                                      pages: 544,
+                                      publication_year: 2014)
 
-    book_2 = author_1.books.create!(title: "About Grace",
-                                    pages: 432,
-                                    publication_year: 2015)
+      book_2 = author_1.books.create!(title: "About Grace",
+                                      pages: 432,
+                                      publication_year: 2015)
 
-    expect(author_1.avg_number_pages).to eq(488)
+      expect(author_1.avg_number_pages).to eq(488)
   end
 end
-=======
->>>>>>> ea5a38756f8d72243c5514b67a71499109ddcd82
